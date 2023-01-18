3af9cd7661d1b18d9cb5f6c64f0657cd938d1bac
Bradley McFadden <bmcfadden@laurentian.ca>
2023-01-17 22:14:34 -0500

    Add missing Tween

--- levels/level1/hidden_area.gd
+++ levels/level1/hidden_area.gd
@@ -4,7 +4,7 @@ extends Node2D
 func _on_BreakableWall2_broke(is_broken: bool) -> void:
 	# print("Breakable wall broke! ", str(is_broken))
 	if is_broken:
-		$Tween.interpolate_property(self, modulate, null, Color.transparent, 1.0)
+		$Tween.interpolate_property(self, "modulate", null, Color.transparent, 1.0)
 		$Tween.start()
 		$Revealed.play()
 		yield($Tween, "tween_completed")
